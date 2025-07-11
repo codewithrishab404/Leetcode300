3996db1a28f3be1ef2dc081d8a50cd48faefb1f4
Rishab <rishabmallick20004@gmail.com>
2025-07-12 00:45:36 +0530

    Solution of leetcode problem #80 is added

---
+++ Arrays/80. Remove Duplicates from Sorted Array II/Solution.java
@@ -0,0 +1,13 @@
+// Solution Code:
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int count =2;
+        for (int i =2;i<nums.length;i++){
+            
+            if(nums[i]!=nums[count-2])
+            nums[count++]=nums[i];
+        }
+        return count;
+    }
+}
