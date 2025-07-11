a3cadf38d9a85c615811bc6cfc2ddcee0e21b585
Rishab <rishabmallick20004@gmail.com>
2025-07-11 21:49:33 +0530

    Optimized code of leetcode 189 problem rotate array is added

--- Arrays/189.Rotate Array/Solution.java
+++ Arrays/189.Rotate Array/Solution.java
@@ -26,3 +26,24 @@ class Solution {
     }
 }
 // Above Brute force approach , as it named brute force approach , its not optimized , it is not space efficient
+
+
+// Optimized code :
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k=k%nums.length; // this line will handle the case when k > nums.length
+       reverse(nums,0,nums.length-1); // Reverse the whole array
+       reverse(nums,0,k-1);   // Reverse first k elements
+       reverse(nums,k,nums.length-1);  // Reverse remaining n-k elements
+
+    }
+    public static void reverse(int[] arr ,int left, int right){
+  
+        while(left<right){
+            int temp=arr[left];
+            arr[left++]= arr[right];
+            arr[right--]= temp;
+        }
+    }
+}
